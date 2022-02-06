61d2ced87e01f0827f8858b5580cbee59101225e
ghoulhex <juan.ign.1999@gmail.com>
2022-02-06 18:47:02 -0300

    desafio complementario

--- desafio-complementario-1/script.js
+++ desafio-complementario-1/script.js
@@ -1,3 +1,5 @@
-if {
-    git
+// Bucle en el que se cuenta desde el numero ingresado hasta el 100
+
+for (let i = parseInt(prompt("Ingrese un numero del 1 al 100")); (i >= 1) && (i <= 100) && (isNaN = i); i++) {
+    console.log(i);
 }
