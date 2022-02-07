6d42fc5438abd4c653c7cc46c968bcaa46ea1358
ghoulhex <juan.ign.1999@gmail.com>
2022-02-06 23:41:06 -0300

    funcion

--- desafio-obligatorio-1/script.js
+++ desafio-obligatorio-1/script.js
@@ -1,75 +1,11 @@
-// Saludar al cliente
-// Mencionar las peliculas disponibles
-// En base a lo que eligio, calcular el precio
-// Informar al cliente el costo que debe abonar
-// Cobrar el servicio
+const sumarNumeros = () => {
 
-const saludar = () => {
-    alert("Bienvenido a Blockbuster")
-};
+    let numero1 = parseFloat(prompt("Ingrese un numero"))
+    let numero2 = parseFloat(prompt("Ingrese otro numero para sumarlo"))
 
-const peliculasDisponibles = () => {
-    let peliculaID;
+    let suma = numero1 + numero2;
 
-    do {
-        peliculaID = parseInt(
-            prompt("Que pelicula alquilara?\n1) El Padrino \n2) Greenbook\n3) Harry Potter\n4) Relatos Salvajes")
-            )
-    } while (peliculaID < 1 || peliculaID > 4 || isNaN(peliculaID));
-
-    let peliculaTitulo;
-
-    switch (peliculaID) {
-        case 1:
-            peliculaTitulo = "El Padrino";
-            break;
-        case 2:
-            peliculaTitulo = "Greenbook";
-            break;
-        case 3:
-            peliculaTitulo = "Harry Potter";
-            break;
-        case 4:
-            peliculaTitulo = "Relatos Salvajes";
-            break;          
-    }
-    return peliculaTitulo
+    alert(`La suma de los numeros ingresados es: ${suma}`)
 }
 
-const calcularPrecio = (peliculaTitulo) => {
-	if (peliculaTitulo === "El Padrino") {
-		return 150;
-	} else if (peliculaTitulo === "Greenbook") {
-		return 200;
-	} else if (peliculaTitulo === "Harry Potter") {
-		return 174;
-	} else {
-		return 500;
-	}
-};
-
-let texto = ""
-let total = 0
-
-const informarProducto = (peliculaTitulo, precioAlquiler) => {
-texto += `Pelicula : ${peliculaTitulo}\ nAlquiler: $${precioAlquiler}\n`;
-total += precioAlquiler;
-let seguir = confirm("Desea alquilar otra pelicula?");
-if (seguir === true) {
-	let peliculaTitulo = peliculasDisponibles();
-	let precioAlquiler = calcularPrecio(peliculaTitulo);
-	informarProducto(peliculaTitulo, precioAlquiler);
-} else {
-    alert("Usted lleva:")
-    alert(texto)
-    alert(`El total para abonar es de $${total}`)
-}
-
-
-
-}
-
-saludar();
-let peliculaTitulo = peliculasDisponibles();
-let precioAlquiler = calcularPrecio(peliculaTitulo);
-informarProducto(peliculaTitulo, precioAlquiler)
+sumarNumeros();
